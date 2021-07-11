57d31596332544580a8f51904bfd51fdda31f559
aghaseyed1 <63206853+aghaseyed1@users.noreply.github.com>
2021-07-11 23:55:51 +0430

    Add files via upload

---
+++ simple hash function.py
@@ -0,0 +1,16 @@
+import random ,string
+
+character=8
+
+def hash_fun(strs):
+    seed=0
+    str_hash=''
+    for s in strs:
+        seed += ord(s)* strs.index(s)+42
+    random.seed(seed)
+    for x in range(character):
+        str_hash+=random.choice(string.hexdigits[:16])
+    return str_hash
+
+
+#hash_fun('hi') --> 'f843c001'
