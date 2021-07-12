42046907b853c62729846fad1b8ff587a018270d
aghaseyed1 <63206853+aghaseyed1@users.noreply.github.com>
2021-07-12 23:14:08 +0430

    Update simple hash function.py

--- simple hash function.py
+++ simple hash function.py
@@ -5,12 +5,12 @@ character=8
 def hash_fun(strs):
     seed=0
     str_hash=''
-    for i,c in enumerate(strs):
-        seed += ord(c)* i + 42
+    for s in strs:
+        seed += ord(s)* ( strs.index(s)+42 )
         
     random.seed(seed)
     for x in range(character):
         str_hash+=random.choice(string.hexdigits[:16])
     return str_hash
 
-print(hash_fun('hi'))
+#print(hash_fun('hi'))
