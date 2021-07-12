a98424df29df6f15f4bfcbeb1c6c9acd2991fe7d
ali0gamer1 <69399548+ali0gamer1@users.noreply.github.com>
2021-07-12 09:12:36 +0430

    replaced str.index with enumerate

--- simple hash function.py
+++ simple hash function.py
@@ -5,11 +5,12 @@ character=8
 def hash_fun(strs):
     seed=0
     str_hash=''
-    for s in strs:
-        seed += ord(s)* strs.index(s)+42
+    for i,c in enumerate(strs):
+        seed += ord(c)* i + 42
+        
     random.seed(seed)
     for x in range(character):
         str_hash+=random.choice(string.hexdigits[:16])
     return str_hash
 
-#hash_fun('hi') --> 'f843c001'
+print(hash_fun('hi'))
